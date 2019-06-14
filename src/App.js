4fa99bc3cc0ad02f867abb6c3194b3deb0d05f62
Lee Graham <butchqueenindrags@Lees-MacBook-Pro.local>
2019-06-14 12:33:05 -0600

    Create routes and setup logic for ProductContainers

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import './App.css';
 class App extends Component {
   constructor() {
     super()
-
     this.state = {
       store: {}
     };
@@ -41,11 +40,35 @@ class App extends Component {
     document.querySelector('.Home').removeAttribute('hidden')
   }
 
+  exportStore() {
+    return this.state
+  }
+
   render() {
     const types = Object.keys(this.state.store)
     const navigation = types.map((type, index) => {
-        return <NavLink to={"/"+type} key={index} className='nav'>{type.toUpperCase().replace('_', ' ')}</NavLink>
+      return (
+        <NavLink to={"/"+type} key={index} className='nav'>
+          {type.toUpperCase().replace('_', ' ')}
+        </NavLink>
+      )
     });
+    var counter = 0
+    const productContainer = (type) => {
+        if(types.length) {
+          var temp = this.state.store[`${type}`]
+          const displayProducts = temp.map((product, i) => {
+            console.log(product)
+            // <ImageCard {...product} key={product.name + i} className='app-img'/>
+          })
+        }
+        // return (
+        //   <div className='image-display'>
+        //     <h1>{product_type.toUpperCase().replace('_', ' ')}!</h1>
+        //     {displayProducts}
+        //   </div>
+        // )
+    }
 
     return (
       <div className="App">
@@ -60,6 +83,16 @@ class App extends Component {
           </form>    
         </div>
         <Route exact path='/' component={Home} />
+        <Route to='/blush' component={productContainer('blush')} />
+        <Route to='/bronzer' component={productContainer('bronzer')} />
+        <Route to='/eyebrow' component={productContainer('eyebrow')} />
+        <Route to='/eyeliner' component={productContainer('eyeliner')} />
+        <Route to='/eyeshadow' component={productContainer('eyeshadow')} />
+        <Route to='/foundation' component={productContainer('foundation')} />
+        <Route to='/lip_liner' component={productContainer('lip_liner')} />
+        <Route to='/lipstick' component={productContainer('lipstick')} />
+        <Route to='/mascara' component={productContainer('mascara')} />
+        <Route to='/nail_polish' component={productContainer('nail_polish')} />
       </div>
     )
   }
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Home = () => {
+
   return (
     <div className='Home' hidden={true}>
       <h2>Click on the links above to see our variety of products</h2>
