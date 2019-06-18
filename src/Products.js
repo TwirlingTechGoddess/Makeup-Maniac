6a5993738e5db74ab9421a69891eededd199413d
Lee Graham <butchqueenindrags@Lees-MacBook-Pro.local>
2019-06-17 18:54:26 -0600

    Limit number of displayed cards to 10

--- src/Products.js
+++ src/Products.js
@@ -4,15 +4,27 @@ import ThumbnailCard from './ThumbnailCard';
 class Products extends Component {
   constructor(props){
     super(props)
+    this.state = {
+      numCards: 10
+    }
   }
 
+
+
   render() {
-    const displayProducts = this.props.products.map((product, i) => 
-      <ThumbnailCard {...product} key={product.id} className='app-img'/>
-    )
+    
+    const displayProducts = this.props.products.reduce((accu, product, i) => {
+      const thumbnail = <ThumbnailCard {...product} key={product.id} className='app-img'/>
+      if(i < this.state.numCards){
+        return [...accu, thumbnail]
+      }
+      return accu
+    },[])
+
     return (
       <div className='product-display'>
         {displayProducts}
+
       </div>
     )
   }
--- src/ThumbnailCard.js
+++ src/ThumbnailCard.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 const ThumbnailCard = ({ brand, name, description, image_link, id, product_type }) => {
 
   return (
-      <Link to={`${product_type}/${id}`}
-            className='Thumbnail'>
-        <h2>{name}</h2>
-        <h3>by {brand}</h3>
-        <img src={image_link}
-             alt='product thumbnail'
-             className='app-img' />
-      </Link>
+    <Link to={`${product_type}/${id}`}
+          className='Thumbnail'>
+      <h2>{name}</h2>
+      <h3>by {brand}</h3>
+      <img src={image_link}
+           alt='product thumbnail'
+           className='app-img' />
+    </Link>
   )
 }
 
