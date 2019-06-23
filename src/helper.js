8b8dbfbd1340ccce1bc129108d74f4a61aff8c1f
Lee Graham <butchqueenindrags@Lees-MacBook-Pro.local>
2019-06-22 18:54:50 -0600

    Implement cleanData function in helper file

--- src/helper.js
+++ src/helper.js
@@ -1,11 +1,21 @@
 export const cleanData = (data) => {
-      const store = data.reduce((accu, cosmetic) => {
-      const type = cosmetic.product_type
-      const newObj = Object.assign({}, )
-      if(!accu[type]){
-        accu[type] = [];
-      }
-      accu[type].push(cosmetic)
-      return accu
-    }, {})
+  const store = data.reduce((accu, cosmetic) => {
+    const type = cosmetic.product_type
+    const newProductObj = Object.assign({}, {
+                          image: cosmetic.api_featured_image,
+                          brand: cosmetic.brand ? cosmetic.brand : '',
+                          description: cosmetic.description,
+                          id: cosmetic.id,
+                          name: cosmetic.name,
+                          colors: cosmetic.product_colors,
+                          type: type,
+                          tags: cosmetic.tag_list
+                          })
+    if(!accu[type]){
+      accu[type] = [];
+    }
+    accu[type].push(newProductObj)
+    return accu
+  }, {})
+  return store
 }
